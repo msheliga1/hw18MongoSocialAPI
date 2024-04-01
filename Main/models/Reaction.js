443c270a5f8217311f84ce955d318e5de85bceb7
Mike Sheliga <msheliga1@gmail.com>
2024-03-31 22:53:25 -0400

    Created Reaction model

---
+++ Main/models/Reaction.js
@@ -0,0 +1,35 @@
+// MJS 3.31.24 - from URI mp 18-28.  A reaction is really a comment on a post. 
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionName: {
+      type: String,
+      required: true,
+      maxlength: 50,
+      minlength: 4,
+      default: 'Unnamed reaction',
+    },
+    score: {
+      type: Number,
+      required: true,
+      default: () => Math.floor(Math.random() * (100 - 70 + 1) + 70),
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
