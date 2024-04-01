6d6e68c392580b99dc6eb4f644f912ae3946d014
Mike Sheliga <msheliga1@gmail.com>
2024-03-31 23:26:12 -0400

    added seeds user-thought seed data. Inserted seed2 in package.json

---
+++ Main/utils/seed2.js
@@ -0,0 +1,57 @@
+const connection = require('../config/connection');
+const { User, Thought } = require('../models');
+const { getRandomName, getRandomAssignments } = require('./data');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+  console.log('connected');
+    // Delete the collections if they exist
+    let userCheck = await connection.db.listCollections({ name: 'users' }).toArray();
+    if (userCheck.length) {
+      await connection.dropCollection('users');
+    }
+
+    let thoughtsCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray();
+    if (thoughtsCheck.length) {
+      await connection.dropCollection('thoughts');
+    }
+
+
+  // Create empty array to hold the thoughts
+  const thoughts = [];
+
+  // Loop 20 times -- add thoughts to the thoughts array
+  for (let i = 0; i < 3; i++) {
+    // Get some random assignment objects using a helper function that we imported from ./data
+    const assignments = getRandomAssignments(4);
+
+    const fullName = getRandomName();
+    const first = fullName.split(' ')[0];
+    const last = fullName.split(' ')[1];
+    const github = `${first}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
+
+    thoughts.push({
+      first,
+      last,
+      github,
+      assignments,
+    });
+  }
+
+  // Add thoughts to the collection and await the results
+  const thoughtData = await Thought.insertMany(thoughts);
+
+  // Add users to the collection and await the results
+  // insertOne -> create per class and my code.
+  await User.create({
+    userName: 'UCLA',
+    inPerson: false,
+    thoughts: [...thoughtData.map(({_id}) => _id)],
+  });
+
+  // Log out the seed data to indicate what should appear in the database
+  console.table(thoughts);
+  console.info('Seeding User->Thought complete! 🌱');
+  process.exit(0);
+});
