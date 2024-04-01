c35cf6bd3ee3ba63d10fce6ad0045fad2704a413
Mike Sheliga <msheliga1@gmail.com>
2024-04-01 11:12:13 -0400

    Created thought routes, controller, put in index. Tested get thought thought/id. Works. Yipeee!!

---
+++ Main/routes/api/thoughtRoutes.js
@@ -0,0 +1,24 @@
+// MJS 3.31.24 - modified from studentRoutes mp 18-28. 
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  deleteThought,
+  addAssignment,
+  removeAssignment,
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getSingleThought).delete(deleteThought);
+
+// /api/thoughts/:thoughtId/assignments
+router.route('/:thoughtId/assignments').post(addAssignment);
+
+// /api/thoughts/:thoughtId/assignments/:assignmentId
+router.route('/:thoughtId/assignments/:assignmentId').delete(removeAssignment);
+
+module.exports = router;
---
+++ Main/routes/api/userRoutes.js
@@ -0,0 +1,21 @@
+// MJS 3.31.24 - Modified from mp18-28. 
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require('../../controllers/userController.js');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router
+  .route('/:userId')
+  .get(getSingleUser)
+  .put(updateUser)
+  .delete(deleteUser);
+
+module.exports = router;
