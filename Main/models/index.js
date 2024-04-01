ec685c52618692e4799dcc3be54a16ee327ad5d4
Mike Sheliga <msheliga1@gmail.com>
2024-04-01 10:39:54 -0400

    Corrected a-has-b order

--- Main/models/index.js
+++ Main/models/index.js
@@ -3,4 +3,4 @@ const Student = require('./Student');
 const User = require('./User');
 const Thought = require('./Thought');
 
-module.exports = { Student, Course, User, Thought };
+module.exports = { Course, Student, User, Thought };
